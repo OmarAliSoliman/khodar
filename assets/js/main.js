75d48583035f3eb53c9071bfed370f3edf733a39
OmarAliSoliman <omarasmar9955@gmail.com>
2020-08-17 15:13:04 +0200

    new-page

--- assets/js/main.js
+++ assets/js/main.js
@@ -41,6 +41,23 @@ $(document).ready(function () {
   }
 
 
+  $('.slider-top').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: true,
+    fade: true,
+    dots: false,
+    asNavFor: '.slider-bottom'
+  });
+  $('.slider-bottom').slick({
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    asNavFor: '.slider-top',
+    dots: false,
+    centerMode: false,
+    focusOnSelect: true
+  });
+
   if ($('.departments').length) {
     let plusBtn = document.querySelectorAll('.departments .card .plus');
     let departmentCard = document.querySelectorAll('.departments .card');
